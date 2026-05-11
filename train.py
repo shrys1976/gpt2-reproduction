5cd5cd5c12630aa62db197a7f04a77b5bc814880
shrys <shreyasjain2018@gmail.com>
2026-05-11 12:48:43 +0000

    feat(train): add GPT forward pass and tiktoken sampling

--- train.py
+++ train.py
@@ -97,6 +97,20 @@ class GPT(nn.Module):
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias = False)
 
 
+     def forward(self, idx):
+         B,T = idx.size()
+         pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+         tok_emb = self.transformer.wte(idx)
+         pos_emb = self.transformer.wpe(pos)
+         x = tok_emb + pos_emb
+         for block in self.transformer.h:
+             x = block(x)
+         x = self.transformer.ln_f(x)
+         logits = self.lm_head(x)
+         return logits
+
+
+
      @classmethod        
      def from_pretrained(cls, model_type):
         """Loads pretrained GPT-2 model weights from Hugging Face."""
@@ -142,5 +156,38 @@ class GPT(nn.Module):
         return model
 
 
-model = GPT.from_pretrained('gpt2')
-print(model)
+
+num_return_sequences = 5
+max_length = 30
+model = GPT(GPTConfig())
+model.eval()
+model.to('cuda')
+
+import tiktoken
+
+enc = tiktoken.getencodig('gpt2')
+tokens =enc.encode("Hello, I'm a language model")
+tokens =torch.tensor(tokens, dtype = torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens.to('cuda')
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+while x.size(1) < max_length:
+    with torch.nograd():
+        logits = model(x)
+        logits = logits[:,-1,:]
+        probs = F.softmax(logits, dim=-1)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim = -1)
+        ix = torch.multinomial(topk_probs, 1)
+        xcol = torch.gather(topk_indices, -1, ix)
+        x = torch.cat((x, xcol),dim =1)
+
+
+
+for i in range(num_return_sequences):        
+    tokens =x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
+
